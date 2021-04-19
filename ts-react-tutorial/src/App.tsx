646c1f9fce1d08905c70311c7a1bc8d6179a609a
onethegarden <onethegarden@gmail.com>
2021-04-20 06:27:40 +0900

    typescript useState

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Greetings from './Greeting';
+import React from "react";
+import Greetings from "./Greeting";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
 
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} say hello`);
-  }
+  };
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
   return (
-    <Greetings name="Hello" onClick={onClick}/>
+    <>
+      <Greetings name="Hello" onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit} />;
+    </>
   );
 }
 
