fe137a5e2e20abd5dfe1407b7fea3a252e112f76
onethegarden <onethegarden@gmail.com>
2021-05-26 07:44:44 +0900

    typescript thunk 적용1

--- ts-react-redux-tutorial/src/App.tsx
+++ ts-react-redux-tutorial/src/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import CounterContainer from "./containers/CounterContainer";
 import TodoContainer from "./containers/TodoApp";
 import GreetContainer from "./containers/GreetContainer";
+import GithubProfileLoader from "./containers/GithubProfileLoader";
 
 function App() {
   return (
     <>
-      <GreetContainer />
-      <CounterContainer />
-      <TodoContainer />
+      <GithubProfileLoader />
     </>
   );
 }
--- ts-react-redux-tutorial/src/modules/index.ts
+++ ts-react-redux-tutorial/src/modules/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import counter from "./counter";
 import todos from "./todos";
 import greet from "./greet";
+import github from "./github";
 
 const rootReducer = combineReducers({
   counter,
   todos,
   greet,
+  github,
 });
 
 //루트 리듀서 내보내기
